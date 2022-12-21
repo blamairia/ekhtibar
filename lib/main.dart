974a5743d25fee62e35f5f6b3699a8b89d77247e
Billel Lamairia <blamairia@gmail.com>
2022-12-21 18:56:22 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,155 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QuizApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: Colors.pink[50],
+        appBar: AppBar(
+          title: const Text('Quizz'),
+          backgroundColor: Colors.purple[400],
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(24.0),
+          child: QuizPage(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<QuizPage> createState() => _QuizPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> anwserResult = [];
 
-  void _incrementCounter() {
+  void checkAnswer(bool asnwer) {
+    bool correctAnswer = questionGroupe[questionNumber].questionAnswer;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (asnwer == correctAnswer) {
+        anwserResult.add(
+          const Padding(
+            padding: EdgeInsets.all(3.0),
+            child: Icon(
+              Icons.thumb_up_off_alt_sharp,
+              color: Colors.green,
+            ),
+          ),
+        );
+      } else {
+        anwserResult.add(
+          const Padding(
+            padding: EdgeInsets.all(3.0),
+            child: Icon(
+              Icons.thumb_down,
+              color: Colors.red,
+            ),
+          ),
+        );
+      }
+      questionNumber++;
     });
   }
 
+  List<Question> questionGroupe = [
+    Question('the number of the planets in our solar system is 8',
+        'images/image-1.jpg', true),
+    Question('Cats eat meat', 'images/image-2.jpg', true),
+    Question('China is located in Africa', 'images/image-3.jpg', false),
+    Question('The earth is Flat', 'images/image-4.jpg', false),
+    Question('The earth is Flat', 'images/image-5.jpg', false),
+    Question('The earth is Flat', 'images/image-6.jpg', false),
+    Question('The earth is Flat', 'images/image-7.jpg', false),
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          children: anwserResult,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Expanded(
+          flex: 5,
+          child: Column(
+            children: [
+              Image.asset(
+                questionGroupe[questionNumber].questionImage,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                questionGroupe[questionNumber].questionText,
+                style: const TextStyle(
+                  fontSize: 25,
+                ),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: TextButton(
+              onPressed: () {
+                checkAnswer(true);
+              },
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.indigoAccent,
+              ),
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: TextButton(
+              onPressed: () {
+                checkAnswer(false);
+              },
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: const Color.fromARGB(255, 237, 107, 21),
+              ),
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
