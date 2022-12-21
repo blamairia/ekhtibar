666dad89e19583589b6767f106e0f9c32bf1f908
Billel Lamairia <blamairia@gmail.com>
2022-12-21 18:56:42 +0100

    5

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String questionText = '';
+  String questionImage = '';
+  bool questionAnswer = true;
+
+  Question(String i, String c, bool j) {
+    questionText = i;
+    questionImage = c;
+    questionAnswer = j;
+  }
+}
